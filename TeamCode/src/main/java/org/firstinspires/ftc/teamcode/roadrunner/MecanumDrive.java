779bcadc24a1f9e8e1a12e1700acdf5937df79fd
outsharded <92227828+outsharded@users.noreply.github.com>
2024-05-30 14:52:43 +0100

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -62,28 +62,28 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.UP;
 
         // drive model parameters
-        public double inPerTick = 0.024997396104572;
-        public double lateralInPerTick = 0.026484219819024;
+        public double inPerTick = 0.024749026838763;
+        public double lateralInPerTick = 0.026290059750136;
         public double trackWidthTicks = 1191.6820442647947;
 
         // feedforward parameters (in tick units)
-        public double kS = 3.7321921716757815;
+        public double kS = 3.732191716757815;
         public double kV = 0.002550517386004112;
-        public double kA = 0.0009;
+        public double kA = 0.00042;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 50;
+        public double maxWheelVel = 30;
         public double minProfileAccel = -30;
-        public double maxProfileAccel = 50;
+        public double maxProfileAccel = 40;
 
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 2.0;
-        public double lateralGain = 2.0;
-        public double headingGain = 1.0; // shared with turn
+        public double axialGain = 3.5;
+        public double lateralGain = 2.5;
+        public double headingGain = 1.5; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
