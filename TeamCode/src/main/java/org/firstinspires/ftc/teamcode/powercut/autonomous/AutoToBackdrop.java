06da64001557e3bbf7eb2df8e03985a19153d61f
outsharded <92227828+outsharded@users.noreply.github.com>
2024-05-30 14:52:31 +0100

    Create AutoToBackdrop.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powercut/autonomous/AutoToBackdrop.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode.powercut.autonomous;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.powercut.hardware.ArmSystem;
+import org.firstinspires.ftc.teamcode.powercut.hardware.DroneSystem;
+import org.firstinspires.ftc.teamcode.powercut.vision.VisionSystem;
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+
+@Config
+@Autonomous(name = "BackBlueAutoToBackdrop", preselectTeleOp = "Drive")
+public class AutoToBackdrop extends OpMode {
+    private MecanumDrive drive;
+    private VisionSystem visionSystem = new VisionSystem();
+    private ArmSystem arm = new ArmSystem();
+    private DroneSystem droneSystem = new DroneSystem();
+
+    // monitoring
+    private int position = 0;
+
+    private Action toBackdrop;
+    private Action park;
+    @Override
+    public void init() {
+        drive = new MecanumDrive(hardwareMap, new Pose2d(12, -63, Math.toRadians(90)));
+        arm.init(hardwareMap);
+        visionSystem.init(hardwareMap);
+        droneSystem.init(hardwareMap);
+
+        telemetry.addLine("Init hardware maps");
+        telemetry.update();
+
+        arm.gripLeftActivate();
+        arm.gripRightActivate();
+        //droneSystem.preset();
+
+        telemetry.addLine("Init hardware positions");
+        telemetry.update();
+
+
+
+        toBackdrop = drive.actionBuilder(drive.pose)
+                .splineTo(new Vector2d(38.5, -48), Math.toRadians(180))
+                .build();
+
+        park = drive.actionBuilder(drive.pose)
+                .strafeTo(new Vector2d(40, -62))
+                .build();
+
+        telemetry.addLine("Init paths. Fully Initialised.");
+        telemetry.update();
+
+    }
+
+    @Override
+    public void start() {
+      Actions.runBlocking(new SequentialAction(
+              toBackdrop,
+              new SleepAction(1),
+              arm.armUp(),
+              new SleepAction(1),
+              arm.gripTuck(),
+              new SleepAction(1),
+              new ParallelAction(arm.armToResetPosition(), park)
+      ));
+
+        // to backdrop
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Pose", "%4.2f, %4.2f, %4.1f", drive.pose.position.x, drive.pose.position.y, drive.pose.heading.real);
+        telemetry.addData("Detected position", position);
+        telemetry.update();
+    }
+}
