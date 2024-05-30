ebc7dd52d8c289f66fa6c24e8bd2064d3031ea13
outsharded <92227828+outsharded@users.noreply.github.com>
2024-05-30 14:52:29 +0100

    Update AutoNonLinear.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powercut/autonomous/AutoNonLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/powercut/autonomous/AutoNonLinear.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.powercut.autonomous;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -13,6 +15,7 @@ import org.firstinspires.ftc.teamcode.powercut.hardware.DroneSystem;
 import org.firstinspires.ftc.teamcode.powercut.vision.VisionSystem;
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 
+@Config
 @Autonomous(name = "MainAuto", preselectTeleOp = "Drive")
 public class AutoNonLinear extends OpMode {
     private MecanumDrive drive;
@@ -32,32 +35,53 @@ public class AutoNonLinear extends OpMode {
         arm.init(hardwareMap);
         visionSystem.init(hardwareMap);
         droneSystem.init(hardwareMap);
-        droneSystem.preset();
 
-        Actions.runBlocking(arm.gripActivate());
+        telemetry.addLine("Init hardware maps");
+        telemetry.update();
+
+        arm.gripLeftActivate();
+        arm.gripRightActivate();
+        //droneSystem.preset();
+
+        telemetry.addLine("Init hardware positions");
+        telemetry.update();
+
+
 
         lookLeftSM = drive.actionBuilder(drive.pose)
-                .splineTo(new Vector2d(12, -36), Math.toRadians(90))
-                .splineTo(new Vector2d(12, -36), Math.toRadians(0))
+                .splineTo(new Vector2d(0, -40), Math.toRadians(120))
+                .waitSeconds(0.5)
                 .build();
 
         turnToCentreSM = drive.actionBuilder(drive.pose)
-                .splineTo(new Vector2d(12, -36), Math.toRadians(90))
+                .splineTo(new Vector2d(8, -24), Math.toRadians(0))
+                .waitSeconds(0.5)
                 .build();
 
         turnToRightSM = drive.actionBuilder(drive.pose)
-                .splineTo(new Vector2d(12, -36), Math.toRadians(180))
+                .splineTo(new Vector2d(14, -30), Math.toRadians(-30))
+                .waitSeconds(0.5)
                 .build();
 
+        telemetry.addLine("Init paths. Fully Initialised.");
+        telemetry.update();
 
     }
 
     @Override
     public void start() {
+        telemetry.addLine("Pathing to left SM in progress.");
+        telemetry.update();
+
         Actions.runBlocking(lookLeftSM);
 
+        telemetry.addLine("Path to left SM complete.");
+        telemetry.update();
+
+        Actions.runBlocking(new SleepAction(1000));
         if (visionSystem.isGamepeicePresent()) {
             telemetry.addLine("Gamepeice at SM Left. Placing Pixel");
+            telemetry.update();
             position = 1;
             Actions.runBlocking(
                     new SequentialAction(
@@ -66,10 +90,17 @@ public class AutoNonLinear extends OpMode {
                     )
             );
         } else {
+            telemetry.addLine("Pathing to centre SM in progress.");
+            telemetry.update();
             Actions.runBlocking(turnToCentreSM);
+
+            telemetry.addLine("Path to centre SM Complete.");
+            telemetry.update();
+
             if (visionSystem.isGamepeicePresent()) {
                 position = 2;
                 telemetry.addLine("Gamepeice at SM Centre. Placing Pixel");
+                telemetry.update();
                 Actions.runBlocking(
                         new SequentialAction(
                                 arm.armDown(),
@@ -79,6 +110,7 @@ public class AutoNonLinear extends OpMode {
             } else {
                 position = 3;
                 telemetry.addLine("Gamepeice at SM Right (Proc Of Elim). Placing Pixel");
+                telemetry.update();
                 Actions.runBlocking(
                         new SequentialAction(
                                 turnToRightSM,
