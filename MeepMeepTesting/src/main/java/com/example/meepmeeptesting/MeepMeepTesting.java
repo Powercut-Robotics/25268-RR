63ce231fcc0fadd3d740a9be12042cf8b6337522
outsharded <fab_tec@icloud.com>
2024-06-19 17:41:04 +0100

    Update MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -32,6 +32,22 @@ public class MeepMeepTesting {
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-36, 63.5, Math.toRadians(270)))
                                 .waitSeconds(1)
+                                .splineTo(new Vector2d(-57, 40), Math.toRadians(180))
+                                .strafeTo(new Vector2d(-57,6))
+                                .strafeTo(new Vector2d(50, 6))
+                                .strafeTo(new Vector2d(52, 36))
+                                .waitSeconds(3.5)
+                                .strafeTo(new Vector2d(52, 63))
+                                .strafeTo(new Vector2d(62, 63))
+                                .build()
+                );
+
+        RoadRunnerBotEntity frontBlueRiggingSide = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 16.5)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, 63.5, Math.toRadians(270)))
+                                .waitSeconds(1)
                                 .splineTo(new Vector2d(-24, 58), Math.toRadians(0))
                                 .splineTo(new Vector2d(12, 58), Math.toRadians(0))
                                 .splineTo(new Vector2d(40, 36), Math.toRadians(180))
@@ -120,6 +136,22 @@ public class MeepMeepTesting {
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-36, -63.5, Math.toRadians(90)))
                                 .waitSeconds(1)
+                                .splineTo(new Vector2d(-57, -40), Math.toRadians(180))
+                                .strafeTo(new Vector2d(-57,-6))
+                                .strafeTo(new Vector2d(50, -6))
+                                .strafeTo(new Vector2d(52, -36))
+                                .waitSeconds(3.5)
+                                .strafeTo(new Vector2d(50, -63))
+                                .strafeTo(new Vector2d(62, -63))
+                                .build()
+                );
+
+        RoadRunnerBotEntity frontRedRiggingSide = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(30, 60, Math.toRadians(180), Math.toRadians(180), 16.5)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, -63.5, Math.toRadians(90)))
+                                .waitSeconds(1)
                                 .splineTo(new Vector2d(-24, -58), Math.toRadians(0))
                                 .splineTo(new Vector2d(12, -58), Math.toRadians(0))
                                 .splineTo(new Vector2d(40, -36), Math.toRadians(180))
@@ -130,17 +162,21 @@ public class MeepMeepTesting {
                                 .build()
                 );
 
+
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-//                .addEntity(frontBlueCentre)
+                .addEntity(frontBlueCentre)
                 .addEntity(frontBlueSide)
+                .addEntity(frontBlueRiggingSide)
                 .addEntity(backBlueSide)
-//                .addEntity(backBlueCentre)
-//                .addEntity(frontRedCentre)
-//                .addEntity(frontRedSide)
-                //.addEntity(backRedSide)
-                //.addEntity(backRedCentre)
+                .addEntity(backBlueCentre)
+                .addEntity(frontRedCentre)
+                .addEntity(frontRedRiggingSide)
+                .addEntity(frontRedSide)
+                .addEntity(backRedSide)
+                .addEntity(backRedCentre)
+
                 .start();
     }
 }
